8315fbceb631d1ed43367c9c2b4fa7c3f2d00987
Adnan Ahmad <adnanahmad07@gmail.com>
2016-04-07 12:54:00 +0530

    Fixes #4

--- loop.rb
+++ loop.rb
@@ -1,32 +1,40 @@
+#prob 5
 class Prime_test
-
-	def val_print
-		for x in (100..200)
-			puts x
-		end	
-	end
-
-	def prime(x)
-		check=0
-		for i in(2..(x/2))
-			if(x%i==0)
-				check=1
-				puts " #{x} is Not prime"
-				return
-			end
-		end		
-		puts "#{x} is PRIME"
-	end		
-	
-	def condition_print
-		for x in (60..120)
-			prime(x)
-
-		end		
-
-	end	
+    
+    def val_print
+        for x in (100..200)
+            puts x
+        end
+    end
+    
+    def prime(x)
+        check=0
+        for i in(2..(x/2))       #since range is from 60-120 , special case for 2 not considered
+            if(x%i==0)
+                
+                #puts " #{x} is Not prime"
+                return false
+            end
+        end
+        return true
+        #puts "#{x} is PRIME"
+    end
+    
+    def condition_print
+        for x in (60..120)
+            if(prime(x)==false)
+                puts " #{x} is Not prime"
+                
+                else
+                puts "#{x} is PRIME"
+            end	
+            
+            
+        end		
+        
+    end	
 end
 
 obj=Prime_test.new
 obj.val_print
-obj.condition_print	
+obj.condition_print
