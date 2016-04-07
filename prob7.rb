1b8ca54649ad3f842c8dcb76069747fd4bd5490c
Adnan Ahmad <adnanahmad07@gmail.com>
2016-04-07 12:56:15 +0530

    Fixes #7

--- prob7.rb
+++ prob7.rb
@@ -1,7 +1,8 @@
 # Problem -7 No of occurences in a file
-
+puts "enter the name of the file"
+name=gets.chomp
 h=Hash.new(0)
-file_input=File.open('7_input.txt','r')
+file_input=File.open(name,'r')
 file_input.gets.downcase.scan(/\b[a-z]{1,16}\b/) { |match|  h[match]=h[match]+1}
 puts h
 exit
