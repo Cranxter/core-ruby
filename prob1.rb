591ea97e602f6138c5d232c4a91585072006df10
Adnan Ahmad <adnanahmad07@gmail.com>
2016-04-07 12:51:31 +0530

    Fixes #1

--- prob1.rb
+++ prob1.rb
@@ -1,42 +1,41 @@
 
 # problem-1 Smallest and largest from 2-D array
 
+puts "enter the size of the 2-D array"
+limit=gets.chomp.to_i
+limit=limit-1
+
 a=Hash.new
-(0..2).each do |i|
-	(0..2).each do |j|
-		puts "enter your input for #{i} , #{j}"
-		a[[i,j]]=gets.chomp.to_i
-	end	
+(0..limit).each do |i|
+    (0..limit).each do |j|
+        puts "enter your input for #{i} , #{j}"
+        a[[i,j]]=gets.chomp.to_i
+    end
 end
 
 
-
-(0..2).each do |i|
-	(0..2).each do |j|		
-		puts "#{a[[i,j]]}"
-	end
-end		
 
 max=0
-(0..2).each do |i|
-	(0..2).each do |j|	
-
-		if(max < a[[i,j]])
-			max=a[[i,j]]
-		end
-	end		
+(0..limit).each do |i|
+    (0..limit).each do |j|
+        
+        if(max < a[[i,j]])
+            max=a[[i,j]]
+        end
+    end
 end
 puts "the maximum value is #{max}"
-	
-min=0
-(0..2).each do |i|
-	(0..2).each do |j|	
-		if(min>a[[i,j]])
-			min=a[[i,j]]
-		end
-	end		
+
+min=a[[0,0]]
+(0..limit).each do |i|
+    (0..limit).each do |j|
+        if(min>a[[i,j]])
+            min=a[[i,j]]
+        end
+    end		
 end
 puts "the minimum value is #{min}"
-	
 
+p a	
+p a.class
 
