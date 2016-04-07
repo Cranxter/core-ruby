f336a20acee1e42bccfb2b1848c98c469a3b03c4
Adnan Ahmad <adnanahmad07@gmail.com>
2016-04-07 12:53:13 +0530

    Fixes #3

--- prob3.rb
+++ prob3.rb
@@ -1,21 +1,35 @@
 require 'json'
 
 class Conversion
-	def array_to_json
-
-		a=Array.new
-		h=[[:name,"eden"],[:age,25],[:club,"chelsea"]].to_h
-		puts " JSON DATA : "+h.to_json
-	end	
-
-	def json_to_array
-
-		json_data='{"name":"eden","age":"25","club":"chelsea"}'
-		h=JSON.parse(json_data)
-		arr=h.to_a
-		print "ARRAY DATA :"
-		p arr
-	end
+    def array_to_json
+        
+        a=Array.new
+        h=[[:name,"eden"],[:age,25],[:club,"chelsea"]].to_h
+        puts " JSON DATA : "+h.to_json
+    end
+    
+    def json_to_array
+        final_array=Array.new
+        json_data='[{"value": "494", "name": ["Sloane", "Tanner", "Daria", "Nissim"]},{"value": "306", "name": ["Ahmed, Emery, Ulric, Risa"]},{"value": "459", "name": ["Hamilton", "Malik", "Reed", "Eve"]}]'
+        #json_data='{"name":"eden","age":"25","club":"chelsea"}'
+        a=JSON.parse(json_data)
+        final_array.push(a[0].keys)
+        
+        a.each do |i|
+            temp_array=Array.new(0)
+            h=i.values
+            temp_array.push(h[0])
+            h[1].each do |j|
+                temp_array.push(j)
+            end
+            final_array.push(temp_array)
+            
+        end	 
+        
+        print "ARRAY DATA :"
+        p final_array
+        
+    end
 end
 obj=Conversion.new
 obj.array_to_json
