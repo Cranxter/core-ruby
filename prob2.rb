10c8aa6eebead9f233288fe74543c71b3c5510d9
Adnan Ahmad <adnanahmad07@gmail.com>
2016-04-07 12:53:04 +0530

    Fixes #2

--- prob2.rb
+++ prob2.rb
@@ -1,24 +1,7 @@
-#Problem 2 - No of days in a month 
+require 'time'
 
-class Months
-	def no_of_days()
-		puts "enter the month to be verified"
-		month=gets.chomp
-		month=month.downcase
-
-		case month
-		when "january","march","may","july","august","october","december"
-			puts "The no days in #{month} are 31"
-		when "april","june","september","november"
-			puts "The no of days in #{month} are 30"	
-		when "february"
-			puts "The no of days in #{month} are 28"
-		else
-			puts "Enter valid input"	
-
-		end
-	end
-end
-
-obj=Months.new
-obj.no_of_days
+time=Time.new
+year=time.year
+month=time.month
+max_day=Date.new(year,month,-1).day
+puts "No of days in Current months are :#{max_day}"
