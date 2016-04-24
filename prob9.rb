84d82c7abab3dfeafbea67366dd01a541e8cb8b5
Adnan Ahmad <adnanahmad07@gmail.com>
2016-04-25 04:31:35 +0530

    added comments

--- prob9.rb
+++ prob9.rb
@@ -28,9 +28,9 @@ end
 
 puts
 
-re_int=/^[+-]?[0-9]{1,10}$/
+re_int=/^[+-]?[0-9]{1,10}$/ 
 re_float=/^[+-]?[0-9]{1,10}[.][0-9]*$/
-re_date=/^(0[1-9]|1[0-9]|2[0-9]|3[0-1])\/(0[0-9]|1[0-2])\/(19\d\d|20\d\d)$/
+re_date=/^(0[1-9]|1[0-9]|2[0-9]|3[0-1])\/(0[0-9]|1[0-2])\/(19\d\d|20\d\d)$/  # valid for dd-mm-yyyy format
 re_string=/^[0-9]*\s*[a-z A-Z  _@^,!#&\*\+\= \s]{1,}\s*[a-z A-Z 0-9 _@^,!.\/#& \*\+\= \s]*$/
 
 #old_str_regex /^[0-9]*\s*[^0-9-\. \/]{1,}[\w  \s]{1,}$/
@@ -43,7 +43,7 @@ headers.each do |head|
 
 	m1=csv_file[0][head].scan re_int
 	m2=csv_file[0][head].scan re_float
-	m3=csv_file[0][head].scan re_date
+	m3=csv_file[0][head].scan re_date  # date format -> dd/mm/yyyy
 	m4=csv_file[0][head].scan re_string
 	if(m1[0]!=nil )
 		puts "Coloumn #{head} is of Integer data type and Quantitative"
